9e1532ea1da16d4703ad6e5505717ff72085eb67
hemersonsilva <hemerson.silva@apektelecom.com>
2022-11-20 17:34:57 -0300

    Correção do componente da lista de conversas

--- app/src/main/java/com/hemerson/msn/ui/components/ItemChatList.kt
+++ app/src/main/java/com/hemerson/msn/ui/components/ItemChatList.kt
@@ -8,33 +8,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.KeyboardArrowLeft
-import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,19 +33,14 @@ import com.hemerson.msn.R
 import com.hemerson.msn.ui.theme.Attention
 import com.hemerson.msn.ui.theme.Black
 import com.hemerson.msn.ui.theme.Blue
-import com.hemerson.msn.ui.theme.DarkBlue
 import com.hemerson.msn.ui.theme.LightBlue
 import com.hemerson.msn.ui.theme.SimpleWhite
-import com.hemerson.msn.ui.theme.VeryLightBlue
 
 @Composable
 fun ItemChatList(
-    isChatScreen: Boolean = false,
     hasNewMessage: Boolean = false,
     userStatusColor: Color
 ) {
-    val profileStartPadding = if (isChatScreen) 24.dp else 8.dp
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -73,31 +57,14 @@ fun ItemChatList(
                     end = 0.dp,
                     bottom = 24.dp
                 ),
-            verticalAlignment = Alignment.CenterVertically,
+            verticalAlignment = CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
 
-            if (isChatScreen) {
-                IconButton(onClick = { }) {
-                    Box(
-                        modifier = Modifier
-                            .size(32.dp)
-                            .clip(CircleShape)
-                            .background(VeryLightBlue),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.KeyboardArrowLeft,
-                            contentDescription = null,
-                            tint = DarkBlue
-                        )
-                    }
-                }
-            }
 
             Box(
                 Modifier
-                    .padding(start = profileStartPadding)
+                    .padding(start = 8.dp)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.profile_placeholder),
@@ -169,5 +136,5 @@ fun ItemChatList(
 @Preview(showBackground = true)
 @Composable
 fun ItemChatListPreview() {
-    ItemChatList(userStatusColor = Attention, isChatScreen = false)
+    ItemChatList(userStatusColor = Attention)
 }
